24f2324af3b9da7bbf105b53b47755df4255428b
Francisco Casanova Ferro <franciscocasanovaferro@gmail.com>
2020-07-02 20:37:10 -0300

    Add form styles

--- components/loan-form.js
+++ components/loan-form.js
@@ -1,56 +1,71 @@
 import React from 'react'
 import { Formik, Form, Field, ErrorMessage } from 'formik';
 import * as yup from 'yup';
+import styles from './loan-form.module.scss';
 
-const LoanForm = props => (
-  <Formik initialValues={props.initialValues} validationSchema={yup.object().shape({
+
+const Row = ({ children }) => <div className={styles.row}>{children}</div>
+
+const FieldStyled = ({ children }) => <div className={styles.field}>{children}</div>
+
+const ErrorMessageStyled = ({name}) => (
+  <p className={styles.error}>
+    <ErrorMessage name={name} />
+  </p>
+);
+
+
+const LoanForm = ({initialValues, onSubmit, buttonCaption}) => (
+  <Formik initialValues={initialValues} validationSchema={yup.object().shape({
     name: yup.string().required('Debe agregar un nombre'),
     last: yup.string().required('Debe agregar un apellido'),
     gender: yup.string().oneOf(['male', 'female']).required('Debe agregar un género'),
     email: yup.string().email('Debe agregar un email válido').required('Debe agregar un email'),
     id: yup.number().required('Debe agregar un DNI')
-  })} onSubmit={props.onSubmit}>
-    <Form>
-      <div>
-        <label htmlFor="name">Nombre</label>
-        <div>
+  })} onSubmit={onSubmit}>
+    <Form className={styles.form}>
+      <Row>
+        <label htmlFor="name">Nombre:</label>
+        <FieldStyled>
           <Field name="name" />
-          <ErrorMessage name="name" />
-        </div>
-      </div>
-      <div>
-        <label htmlFor="last">Apellido</label>
-        <div>
+          <ErrorMessageStyled name="name" />
+        </FieldStyled>
+      </Row>
+      <Row>
+        <label htmlFor="last">Apellido:</label>
+        <FieldStyled>
           <Field name="last" />
-          <ErrorMessage name="last" />
-        </div>
-      </div>
-      <div>
-        <label htmlFor="gender">Género</label>
-        <div>
+          <ErrorMessageStyled name="last" />
+        </FieldStyled>
+      </Row>
+      <Row>
+        <label htmlFor="gender">Género:</label>
+        <FieldStyled>
           <Field name="gender" as="select">
             <option value=''></option>
             <option value='female'>Mujer</option>
             <option value='male'>Hombre</option>
           </Field>
-        </div>
-        <ErrorMessage name="gender" />
-      </div>
-      <div>
-        <label htmlFor="email">Email</label>
-        <div>
+          <ErrorMessageStyled name="gender" />
+        </FieldStyled>
+      </Row>
+      <Row>
+        <label htmlFor="email">Email:</label>
+        <FieldStyled>
           <Field name="email" />
-          <ErrorMessage name="email" />
-        </div>
-      </div>
-      <div>
-        <label htmlFor="id">DNI</label>
-        <div>
+          <ErrorMessageStyled name="email" />
+        </FieldStyled>
+      </Row>
+      <Row>
+        <label htmlFor="id">DNI:</label>
+        <FieldStyled>
           <Field name="id" />
-          <ErrorMessage name="id" />
-        </div>
+          <ErrorMessageStyled name="id" />
+        </FieldStyled>
+      </Row>
+      <div className={styles.button}>
+        <button type="submit">{buttonCaption || 'Pedir prestamo'}</button>
       </div>
-      <button type="submit">Pedir prestamo</button>
     </Form>
   </Formik>
 );
